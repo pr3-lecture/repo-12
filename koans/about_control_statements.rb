322ff0e8fc9aed00018f79b177b8a50fcd2df8c2
ModuloGames <max.granzow@stud.hs-mannheim.de>
2017-10-18 11:32:46 +0200

    Step 144

--- koans/about_control_statements.rb
+++ koans/about_control_statements.rb
@@ -98,7 +98,7 @@ class AboutControlStatements < Neo::Koan
       result = result * i
       i += 1
     end
-    assert_equal __, result
+    assert_equal 3628800, result
   end
 
   def test_break_statement_returns_values
@@ -110,6 +110,7 @@ class AboutControlStatements < Neo::Koan
 
     assert_equal __, result
   end
+
 
   def test_next_statement
     i = 0
