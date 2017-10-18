d6d6394ad457460a81fdfd21756b460d677f52dd
ModuloGames <max.granzow@stud.hs-mannheim.de>
2017-10-18 12:21:32 +0200

    Step 154

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,7 +14,14 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  if(a == b && a == c)
+    return :equilateral
+  end
+  if(a == b || a == c || b == c)
+    return :isosceles
+  end
+  return :scalene
+
 end
 
 # Error class used in part 2.  No need to change this code.
