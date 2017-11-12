6c6163843f6a07a2fc65901a283c7f400ac411e7
ModuloGames <max.granzow@stud.hs-mannheim.de>
2017-11-12 23:16:42 +0100

    Step 177

--- koans/about_blocks.rb
+++ koans/about_blocks.rb
@@ -68,7 +68,7 @@ class AboutBlocks < Neo::Koan
 
   def test_blocks_can_be_assigned_to_variables_and_called_explicitly
     add_one = lambda { |n| n + 1 }
-    assert_equal __, add_one.call(10)
+    assert_equal 11, add_one.call(10)
 
     # Alternative calling syntax
     assert_equal 11, add_one[10]
@@ -77,7 +77,7 @@ class AboutBlocks < Neo::Koan
   def test_stand_alone_blocks_can_be_passed_to_methods_expecting_blocks
     make_upper = lambda { |n| n.upcase }
     result = method_with_block_arguments(&make_upper)
-    assert_equal "JIM", result
+    assert_equal __, result
   end
 
   # ------------------------------------------------------------------
