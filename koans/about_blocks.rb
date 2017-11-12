ad2f4196e1f0bef4533dc68f565cf82750ee0ea7
ModuloGames <max.granzow@stud.hs-mannheim.de>
2017-11-12 23:15:22 +0100

    Step 174

--- koans/about_blocks.rb
+++ koans/about_blocks.rb
@@ -40,7 +40,7 @@ class AboutBlocks < Neo::Koan
   def test_methods_can_call_yield_many_times
     result = []
     many_yields { |item| result << item }
-    assert_equal __, result
+    assert_equal [:peanut, :butter, :and, :jelly], result
   end
 
   # ------------------------------------------------------------------
@@ -54,7 +54,7 @@ class AboutBlocks < Neo::Koan
   end
 
   def test_methods_can_see_if_they_have_been_called_with_a_block
-    assert_equal :with_block, yield_tester { :with_block }
+    assert_equal __, yield_tester { :with_block }
     assert_equal :no_block, yield_tester
   end
 
